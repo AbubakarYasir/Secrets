58b356a1e57872982f65a8ae79a7a74540e0b0da
Abubakar Yasir <theabubakaryasir@gmail.com>
2022-12-08 14:48:48 +0500

    🔐 Authentication - Login & Register Page added

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 //jshint esversion:6
 const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
+const bodyParser = require("body-parser"); // Body Parser
+const ejs = require("ejs"); // Templating Engine
+const bcrypt = require("bcrypt"); // Hashing Library
 
 const app = express();
-
+// Set the view engine to ejs
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(
@@ -54,6 +55,73 @@ app.get("/submit", function (req, res) {
     res.render("submit");
 });
 
-app.listen(3000, function () {
-    console.log("Server started on http://localhost:" + 3000);
+// Regiser - POST Request
+app.post("/register", function (req, res) {
+    // Hash and Store the User Password
+    bcrypt.hash(req.body.password, 12, function (err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash,
+        });
+        // Find if there is a user with the same email
+        User.findOne({ email: newUser.email }, function (err, foundUser) {
+            if (err) {
+                console.log(err);
+            } else {
+                // If there is no user with the same email, save the new user
+                if (foundUser == null && newUser.email != "") {
+                    newUser.save(function (err) {
+                        if (err) {
+                            console.log(err);
+                        }
+                        // If the user is saved, render the secrets page
+                        else {
+                            res.render("secrets");
+                            console.log("New User has successfully registered:");
+                        }
+                    });
+                }
+                // If there is a user with the same email, render the register page again.
+                else {
+                    res.render("register");
+                    console.log("User already exists!");
+                }
+            }
+        });
+    });
+});
+
+// Login - POST Request
+app.post("/login", function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+    // Find the user with the same email
+    User.findOne({ email: username }, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        }
+        // If the user is found, compare the password
+        else {
+            if (foundUser != null) {
+                bcrypt.compare(password, foundUser.password, function (err, result) {
+                    if (result == true) {
+                        res.render("secrets");
+                    } else {
+                        res.render("login");
+                        console.log("Wrong password!");
+                    }
+                });
+            } // If the user is not found, render the login page again.
+            else if (foundUser == null) {
+                res.render("login");
+                console.log("User not found!");
+            }
+        }
+    });
+});
+
+// Start the server
+const port = 3000 || process.env.PORT;
+app.listen(port, function () {
+    console.log("Server started on http://localhost:" + port);
 });
